a89553a6b93d622700dccf75520f2023a22e4bd8
Phạm Quốc Việt <97594668+phamviet2610@users.noreply.github.com>
2022-06-01 23:26:08 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,24 +6,22 @@
 using namespace std;
 int main() {
 	APhu a; a.input();
-	string loai;
+	int loai;
 	Taykhong z;
 	Vukhi **x= new Vukhi*[a.getSoluongvukhi()];
 	for (int i = 0; i < a.getSoluongvukhi(); i++) {
-		cin.ignore();
-		getline(cin, loai);
-		if (loai == "Dao") x[i] = new Dao();
-		else if (loai == "Cung ten") x[i] = new Cungten();
+		cin >> loai;
+		if (loai == 1) x[i] = new Dao();
+		else if (loai == 2) x[i] = new Cungten();
 		x[i]->input();
 	}
 	int m; cout << "\nNhap vao so luong thu du: "; cin >> m;
 	Thudu** y = new Thudu * [m];
-	string loai1;
+	int loai1;
 	for (int i = 0; i < m; i++) {
-		cin.ignore();
-		getline(cin, loai1);
-		if (loai1 == "Bo tot") y[i] = new Botot();
-		else if (loai1 == "Ho") y[i] = new Ho();
+		cin >> loai1;
+		if (loai1 == 1) y[i] = new Botot();
+		else if (loai1 == 2) y[i] = new Ho();
 		y[i]->input();
 	}
 	int i = 0, j = 0;
@@ -34,14 +32,14 @@ int main() {
 		}
 		else {
 			y[i]->bidanh(x[j]->STsudung());
-			int c = x[j]->getSoLuotSuDung();
-			x[j]->setSoLuotSuDung(c--);
+			int c = x[j]->getSoLuotSuDung()-1;
+			x[j]->setSoLuotSuDung(c);
 			if (c == 0) j++;
 		}
 		if (y[i]->check() > 0) a.bidanh(y[i]->satthuong());
 		else i++;
 	}
-	if (a.getHP() > 0) cout << "A Phu chien thang, hp con lai: " << a.getHP() << ", so luong vu khi con lai: " << a.getSoluongvukhi();
-	else cout << "A Phu that bai, so thu du con lai: " << m-i+1;
+	if (a.getHP() > 0) cout << "A Phu chien thang, hp con lai: " << a.getHP() << ", so luong vu khi con lai: " << a.getSoluongvukhi()-j;
+	else cout << "A Phu that bai, so thu du con lai: " << m-i;
 	return 0;
 }
